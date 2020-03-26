85700c126d873a71ff9d9414c55630717c43dc36
hyojuhan <hyoju4135@gmail.com>
2020-03-26 18:04:39 +0900

    hhj review form 수정 중

--- src/main/java/review/dao/ReviewDAOMybatis.java
+++ src/main/java/review/dao/ReviewDAOMybatis.java
@@ -1,5 +1,28 @@
 package review.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import review.bean.ReviewDTO;
+
+@Repository("reviewDAO")
+@Transactional
 public class ReviewDAOMybatis implements ReviewDAO {
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public List<ReviewDTO> getItemReviewList(int seq) {
+		return sqlSession.selectList("reviewSQL.getItemReviewList", seq);
+	}
+
+	@Override
+	public String getItemName(String item_id) {
+		return sqlSession.selectOne("reviewSQL.getItemName", item_id);
+	}
 
 }

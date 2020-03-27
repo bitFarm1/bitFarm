73df94eb920b5ecd68858aa9e0e23198b3226e5d
hyojuhan <hyoju4135@gmail.com>
2020-03-27 14:27:09 +0900

    hhj/ksj 카카오페이 추가

--- src/main/java/order/controller/OrderController.java
+++ src/main/java/order/controller/OrderController.java
@@ -6,20 +6,23 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import cart.bean.CartListDTO;
 import member.bean.MemberDTO;
+import mypage.bean.MypagePickItemDTO;
 import order.service.OrderService;
 
 @Controller
+@RequestMapping(value="order")
 public class OrderController {
 	@Autowired
 	private OrderService orderService;
 	
-	@RequestMapping(value="/order/orderForm", method=RequestMethod.GET)
+	@RequestMapping(value="orderForm", method=RequestMethod.GET)
 	public ModelAndView orderForm(HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		
@@ -37,4 +40,40 @@ public class OrderController {
 		mav.setViewName("/main/main");
 		return mav;
 	}
+	
+	@RequestMapping(value="pickItemOrder", method=RequestMethod.POST)
+	public ModelAndView pickItemOrder(@ModelAttribute List<MypagePickItemDTO> list) {
+		ModelAndView mav = new ModelAndView();
+		
+		System.out.println(list.size());
+		
+		return mav;
+	}
+	
+	//결제 성공 페이지
+	@RequestMapping(value="orderSuccess", method=RequestMethod.GET)
+	public ModelAndView orderSuccess() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("display", "/order/orderSuccess.jsp");
+		mav.setViewName("/main/main");
+		return mav;
+	}
+	
+	//결제 실패 페이지
+	@RequestMapping(value="orderFail", method=RequestMethod.GET)
+	public ModelAndView orderFail() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("display", "/order/orderFail.jsp");
+		mav.setViewName("/main/main");
+		return mav;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
